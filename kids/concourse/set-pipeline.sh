d50f88731b1ed54231106a4f80a0322f504a5734
Misiego Ruiz, Jose Matías (Cognizant) <JoseMatias.MisiegoRuiz@cognizant.com>
2018-10-19 13:10:02 +0200

    Work on pipeline

--- kids/concourse/set-pipeline.sh
+++ kids/concourse/set-pipeline.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-fly -t rnr login -c http://127.0.0.1:8080 -u admin -p admin
+fly -t rnr login -c http://127.0.0.1:8080 -u test -p test
 fly -t rnr set-pipeline -p coolschool-kids -c pipeline.yml -l .pipeline-config.yml
