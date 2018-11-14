093b1aa3537f1669018e494ee88a27765b02a192
matias <josematias.misiegoruiz@cognizant.com>
2018-11-14 17:10:55 +0100

    enabling discovery client

--- kids/src/main/java/io/coolschool/kids/KidsApplication.java
+++ kids/src/main/java/io/coolschool/kids/KidsApplication.java
@@ -2,9 +2,11 @@ package io.coolschool.kids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @Import({SpringDataRestConfiguration.class})
 public class KidsApplication {
